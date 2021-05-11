3e30aafd9705d959d7ea99275ea2af139a5eb4d0
Koczo Koczownik <mateux111@gmail.com>
2021-05-11 16:43:41 +0200

    zaciągnięcie pluginu i ustawienie aktywacji w projekcie

--- xamarintest/xamarintest.Android/MainActivity.cs
+++ xamarintest/xamarintest.Android/MainActivity.cs
@@ -9,7 +9,8 @@ using Android.OS;
 
 namespace xamarintest.Droid
 {
-    [Activity(Label = "xamarintest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "xamarintest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
