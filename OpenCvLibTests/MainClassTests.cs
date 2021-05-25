22b5d317007620274965c42f2cff50ba1730ab44
Koczo Koczownik <mateux111@gmail.com>
2021-05-25 08:38:57 +0200

    testy dla transforma - dla regularnego kształtu i nietypowego

--- OpenCvLibTests/MainClassTests.cs
+++ OpenCvLibTests/MainClassTests.cs
@@ -136,19 +136,76 @@ namespace OpenCvLib.Tests
         }
 
         [InlineData(10, 1000, 10, 1000)]
+        [InlineData(2000, 1000, 10, 1000)]
+        [InlineData(2000, 4000, 2000, 4000)]
+        [InlineData(8000, 1000, 8000, 1000)]
         [Theory()]
-        public void TransformTest(int xLeft, int xRight, int yUp, int yDown)
+        public void TransformTest_Rectangle(int xLeft, int xRight, int yUp, int yDown)
         {
             using var testObject = MainClass.LoadImage(TestImage);
-            using var result = MainClass.DrawContour(testObject.Clone(), new List<Point>
+
+            var pointsOfFragment = new Point2f[]
+            {
+                new Point2f(xLeft, yUp) ,
+                new Point2f(xRight, yUp) ,
+                new Point2f(xRight, yDown),
+                new Point2f(xLeft, yDown) ,
+            };
+            var pointsOfDestination = new Point2f[]
+            {
+                new Point2f(0, 0),
+                new Point2f(testObject.Width, 0),
+                new Point2f(testObject.Width, testObject.Height),
+                new Point2f(0, testObject.Height)
+            };
+
+            using var contouredImage = MainClass.DrawContour(testObject.Clone(), new List<Point>
             {
                 new Point(xLeft, yUp) ,
                 new Point(xRight, yUp) ,
                 new Point(xRight, yDown),
-                new Point(xLeft, yDown) ,
+                new Point(xLeft, yDown) 
             });
+            using var result = MainClass.Transform(contouredImage, pointsOfFragment, pointsOfDestination, testObject.Width, testObject.Height);
 
-            SaveAndCheckIfSavedCorrect(result, $"{xLeft}x{xRight}x{yUp}x{yDown}");
+
+            SaveAndCheckIfSavedCorrect(contouredImage, $"{xLeft}x{xRight}x{yUp}x{yDown}_orgin");
+            SaveAndCheckIfSavedCorrect(result, $"{xLeft}x{xRight}x{yUp}x{yDown}_result");
+        }
+
+        [InlineData(10, 10, 1000, 1000, 1200, 2000, 500, 1800)]
+        [Theory()]
+        public void TransformTest_UnnormalShape(int point1x, int point1y, int point2x, int point2y, int point3x, int point3y, int point4x, int point4y)
+        {
+            using var testObject = MainClass.LoadImage(TestImage);
+
+            var pointsOfFragment = new Point2f[]
+            {
+                new Point2f(point1x, point1y) ,
+                new Point2f(point2x, point2y) ,
+                new Point2f(point3x, point3y),
+                new Point2f(point4x, point4y) 
+            };
+            var pointsOfDestination = new Point2f[]
+            {
+                new Point2f(0, 0),
+                new Point2f(testObject.Width, 0),
+                new Point2f(testObject.Width, testObject.Height),
+                new Point2f(0, testObject.Height)
+            };
+
+            using var contouredImage = MainClass.DrawContour(testObject.Clone(), new List<Point>
+            {
+                new Point(point1x, point1y) ,
+                new Point(point2x, point2y) ,
+                new Point(point3x, point3y),
+                new Point(point4x, point4y)
+            });
+            using var result = MainClass.Transform(contouredImage, pointsOfFragment, pointsOfDestination, testObject.Width, testObject.Height);
+
+
+            SaveAndCheckIfSavedCorrect(contouredImage, $"{point1x}x{point2x}x{point3x}x{point4x}_orgin");
+            SaveAndCheckIfSavedCorrect(result, $"{point1x}x{point2x}x{point3x}x{point4x}_result");
         }
     }
 }
